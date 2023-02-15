1564a256032fc57e04c0fe15aa47e2c096a39d63
imran12ap76 <77523586+imran12ap76@users.noreply.github.com>
2023-02-15 11:46:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001575525780))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Tamilrockers')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
