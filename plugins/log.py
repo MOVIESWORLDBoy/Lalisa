2a557adc2fe1344ddbec3763cc4082f837988bb4
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-02-19 18:52:19 +0530

    Update log.py

--- plugins/log.py
+++ plugins/log.py
@@ -1,12 +1,28 @@
+import logging
 from pyrogram import Client
 import pyrogram
-import subprocess
 
+# Create a logger for your bot
+logger = logging.getLogger(__name__)
 
-@Client.on_message(pyrogram.filters.command("log"))
+# Configure the logger to write logs to a file
+logging.basicConfig(
+    filename="bot.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s"
+)
+
+# Initialize your Pyrogram client
+
+# Define a command handler for the "/log" command
+@Client.on_message(filters.command("log"))
 def get_logs(client, message):
     try:
-        logs = subprocess.check_output(["journalctl", "-u", "my_bot.service"]).decode()
+        # Read the contents of the log file
+        with open("bot.log", "r") as f:
+            logs = f.read()
+        # Send the log file contents as a text message to the user
         message.reply_text(logs)
     except Exception as e:
+        logger.exception("Failed to get logs")
         message.reply_text("Failed to get logs: {}".format(str(e)))
