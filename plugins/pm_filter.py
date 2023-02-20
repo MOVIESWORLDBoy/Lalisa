9d80a3e1b820291b40a54df854e0c6aa590a8ec6
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-02-20 23:05:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -857,35 +857,39 @@ async def manual_filters(client, message, text=False):
                 try:
                     if fileid == "None":
                         if btn == "[]":
-                            fmsg = await client.send_message(group_id, reply_text, disable_web_page_preview=True)
+                            Joelkb1 = await client.send_message(group_id, reply_text, disable_web_page_preview=True)
+                            await asyncio.sleep(300)
+                            await Joelkb1.delete()
                         else:
                             button = eval(btn)
-                            fmsg = await client.send_message(
+                            Joelkb2 = await client.send_message(
                                 group_id,
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
                             )
+                            await asyncio.sleep(300)
+                            await Joelkb2.delete()
                     elif btn == "[]":
-                        fmsg = await client.send_cached_media(
+                        Joelkb3 = await client.send_cached_media(
                             group_id,
                             fileid,
                             caption=reply_text or "",
                             reply_to_message_id=reply_id
                         )
+                        await asyncio.sleep(300)
+                        await Joelkb3.delete()
                     else:
                         button = eval(btn)
-                        fmsg = await message.reply_cached_media(
+                        Joelkb4 = await message.reply_cached_media(
                             fileid,
                             caption=reply_text or "",
                             reply_markup=InlineKeyboardMarkup(button),
                             reply_to_message_id=reply_id
                         )
-                    
-                    await asyncio.sleep(900)
-                    await fmsg.delete()
-                    
+                        await asyncio.sleep(300)
+                        await Joelkb4.delete()
                 except Exception as e:
                     logger.exception(e)
                 break
