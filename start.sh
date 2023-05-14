1a5a812b945889505198447c48ddb0e0e303d97b
imran12ap76 <77523586+imran12ap76@users.noreply.github.com>
2023-05-14 12:09:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 if [ -z $UPSTREAM_REPO ]
+
 then
+
   echo "Cloning main Repository"
-  git clone https://github.com/imran12ap76/Lalisa.git /Lalisa
+
+  git clone https://github.com/imran12ap76/Lalisa.git /app
+
 else
+
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lalisa
+
+  git clone $UPSTREAM_REPO /app
+
 fi
-cd /Lalisa
+
+cd /app
+
 pip3 install -U -r requirements.txt
-echo "Starting BOT...."
+
+echo "Starting DQ-The-File-Donor...."
+
 python3 bot.py
